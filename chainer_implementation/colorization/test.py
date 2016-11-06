ba60b3265afb445b632f6f95ec35cd77d4d3dcdb
tereka114 <m.d.r.y.h0114@gmail.com>
2016-11-06 09:36:59 +0900

    modify models

--- chainer_implementation/colorization/test.py
+++ chainer_implementation/colorization/test.py
@@ -1,9 +1,23 @@
-#coding:utf-8
+# coding:utf-8
 from __future__ import absolute_import
 from __future__ import unicode_literals
 import argparse
 from skimage import color
+import skimage.io
+from model import ColorfulImageColorizationModel
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i")
-parser.add_argument()
+parser.add_argument("-i", "--input_image")
+parser.add_argument("-w", "--weight_file")
+
+args = parser.parse_args()
+input_path = args.input_image
+
+img_rgb = skimage.io.imread(input_path)
+img_lab = color.rgb2lab(img_rgb)
+img_l = img_lab[:,:, 0]
+
+# create grayscale version of image (just for displaying)
+
+model = ColorfulImageColorizationModel()
+pred = model([img_l])
